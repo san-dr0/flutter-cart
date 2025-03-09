ed7bbaadfdd3e7090bd5899827bc585a1f59e3ce
unknown <lisandro.batiancila@gmail.com>
2025-03-09 21:37:17 +0800

    working qrcode

--- lib/feature/barcodepay/presentation/pages/barcode.page.dart
+++ lib/feature/barcodepay/presentation/pages/barcode.page.dart
@@ -1,6 +1,7 @@
 import 'package:clean_arch2/core/color.dart';
 import 'package:clean_arch2/core/string.dart';
 import 'package:flutter/material.dart';
+import 'package:simple_barcode_scanner/simple_barcode_scanner.dart';
 
 class BarcodePayPage extends StatefulWidget {
   const BarcodePayPage({super.key});
@@ -12,11 +13,22 @@ class BarcodePayPage extends StatefulWidget {
 class _BarcodePayPage extends  State<BarcodePayPage> {
 
   @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: tealColor,
         title: Text(barcodePayTitle),
+      ),
+      body: Column(
+        children: [
+          
+        ],
       ),
     );
   }
